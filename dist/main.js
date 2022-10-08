9995f84b0ce634fe9b985fee05f537226d312850
Jasvaran Mann <mannjesse355@gmail.com>
2022-10-07 19:33:19 -0700

    Initial Commit

---
+++ dist/main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+cosnole.log('test')
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLG1CIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vdG9kby1saXN0LXByb2plY3QvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29zbm9sZS5sb2coJ3Rlc3QnKSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
